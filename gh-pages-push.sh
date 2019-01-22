14d41b8f72737de74d573669644c4d3da016d154
Mickaël Misbach <mickael@misba.ch>
2019-01-22 03:43:29 +0100

    add script to publish to GH

---
+++ gh-pages-push.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+git stash
+git checkout master
+
+git ls-files -z | xargs -0 rm -f
+cp -a docs/. .
+
+git add -u
+git commit -m "medco-documentation compiled HTML update from script"
+git push
+
+git checkout "${CURRENT_BRANCH}"
+git stash pop
